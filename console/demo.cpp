15e7fd5cd3bd923013cc5a6d18f5fdf2895f0d23
rhoms <homsrego@esrf.fr>
2011-12-01 19:24:32 +0100

    included

---
+++ console/demo.cpp
@@ -0,0 +1,288 @@
+#define PCO_ERRT_H_CREATE_OBJECT
+
+#define DATA_NR_FRAMES 50
+#define DATA_EXP_TIME 0.000002
+#define EBUFS 400
+#define INFO_SIZE 5000
+
+
+#undef __BYPASSINLINE__
+
+/***
+#include <stdio.h>
+
+#define DEBUG_H
+#define HWMAXIMAGESIZECALLBACK_H
+
+#include "Compatibility.h"
+#include "PCO_Structures.h"
+#include "PCO_ConvStructures.h"
+#include "Pco_ConvDlgExport.h"
+#include "sc2_SDKStructures.h"
+#include "SC2_CamExport.h"
+#include "sc2_defs.h"
+#include "SC2_SDKAddendum.h"
+#include <math.h>
+#include <time_compat.h>
+
+#include "Debug.h"
+#include "Pco.h"
+#include "..\..\camera\pco\sdk\pco-sdk-113\include\sc2_SDKStructures.h"
+#include "..\..\include\sc2_defs.h"
+#include "..\..\include\PCO_err.h"
+#include "..\..\include\PCO_errt.h"
+*/
+
+#include "Constants.h"
+#include "CtControl.h"
+#include "CtAcquisition.h"
+#include "CtSaving.h"
+#include "HwInterface.h"
+#include "PcoCamera.h"
+#include "PcoInterface.h"
+#include "PcoSyncCtrlObj.h"
+#include "PCO_errt.h"
+#include "PoolThreadMgr.h"
+
+
+#include "load.h"
+
+#pragma pack (4)
+#pragma pack ()
+
+
+
+
+int getlib(void);
+//===============================================================================================================
+//===============================================================================================================
+
+	//; FALSE// Set to TRUE, if you want to allocate the buffer on your own.
+	/************************************************************
+	load SC2 library; please note SC2_Cam.dll has dependency on 
+	sc2_1394.dll, this dll must be present in your dll search path.
+	************************************************************/
+
+/************
+enum DebFormat {
+	DebFmtDateTime		= 1 << 0,  1
+	DebFmtThread		= 1 << 1,  2
+	DebFmtModule		= 1 << 2,  4
+	DebFmtObj			= 1 << 3,  8   
+	DebFmtFunct			= 1 << 4,  10	
+	DebFmtFileLine		= 1 << 5,  20
+	DebFmtType			= 1 << 6,  40 
+	DebFmtIndent		= 1 << 7,  80
+	DebFmtColor			= 1 << 8,  100
+};
+***********/
+#define DEBUG_FORMAT 0x30
+
+/*********
+enum DebType {
+	DebTypeFatal		= 1 << 0,  1
+	DebTypeError		= 1 << 1,  2
+	DebTypeWarning		= 1 << 2,  4
+	DebTypeTrace		= 1 << 3,  8
+	DebTypeFunct		= 1 << 4,  10
+	DebTypeParam		= 1 << 5,  20
+	DebTypeReturn		= 1 << 6,  40
+	DebTypeAlways		= 1 << 7,  80
+};
+***********/
+#define DEBUG_TYPE 0xffffffff
+
+/*********
+enum DebModule {
+	DebModNone		= 1 << 0,
+	DebModCommon		= 1 << 1,
+	DebModHardware		= 1 << 2,
+	DebModHardwareSerial	= 1 << 3,
+	DebModControl		= 1 << 4,
+	DebModEspia		= 1 << 5,
+	DebModEspiaSerial	= 1 << 6,
+	DebModFocla		= 1 << 7,
+	DebModCamera		= 1 << 8,
+	DebModCameraCom		= 1 << 9,
+	DebModTest		= 1 << 10,
+	DebModApplication	= 1 << 11,
+};
+***********/
+#define DEBUG_MODULE 0xffffffff
+
+
+/*********
+#cam=Basler.Camera("192.168.180.2",8000)
+#i=Basler.Interface(cam)
+#c=Core.CtControl(i)
+#s=c.saving()
+#par=s.Parameters()
+#par.directory='/tmp/write_test/t0'
+#par.prefix='seb_test_'
+#par.suffix='.edf'
+#par.fileFormat=1
+#par.savingMode=1
+#s.setParameters(par)
+
+#a = c.acquisition()
+#a.setAcqExpoTime(5)
+#a.setAcqNbFrames(100)
+
+#Core.DebParams.setTypeFlags(0xff)
+#Core.DebParams.setModuleFlags(0xffff)
+***********/
+
+//===============================================================================================================
+//===============================================================================================================
+namespace lima {
+
+int myMain(int argc, char* argv[])
+{
+	char *fnId; fnId = "main";
+
+	//char info[INFO_SIZE+1];
+	char *msg;
+	char errbuffer[EBUFS];
+
+	bool bbufferextern = TRUE;
+
+	CtControl::Status status;
+
+	static int err;
+
+
+	printf("\n\n[%s %d] ======================= %s\r\n", __FILE__, __LINE__, "pco.camera demo program");
+	err = getlib();
+	if (err != 0)
+	{
+		sprintf_s(errbuffer, EBUFS, "\nCan not load SC2 library. Error: %d\n", err);
+		printf(errbuffer);
+		printf("Refer to PCO_err.h for explanation of some error codes\n");
+
+		exit(-1);
+		return -1;
+	}
+	printf("\n\n[%s %d] ======================= %s\r\n", __FILE__, __LINE__, "Libraries loaded");
+  
+
+	//DebParams::setModuleFlags(DEBUG_MODULE);
+	//DebParams::setTypeFlags(DEBUG_TYPE);
+	//DebParams::setFormatFlags(DEBUG_FORMAT);
+
+	msg = "new cam" ; printf("\n\n%s> ======================= [%s %d]\r\n", msg, __FILE__, __LINE__);
+	Pco::Camera *myCam = new Pco::Camera("x") ;
+
+	msg = "new if" ; printf("\n\n%s> ======================= [%s %d]\r\n", msg, __FILE__, __LINE__);
+	Pco::Interface *myIf = new Pco::Interface(myCam) ;
+
+	msg = "new control[begin]" ; printf("\n\n%s> ======================= [%s %d]\r\n", msg, __FILE__, __LINE__);
+	CtControl *myCt = new CtControl(myIf) ;
+	msg = "new control [end]" ; printf("%s\n\n> ======================= [%s %d]\r\n", msg, __FILE__, __LINE__);
+
+	CtAcquisition* 	myCtAcq = myCt->acquisition();
+
+
+#if 1
+	CtSaving::Parameters myPar;
+	CtSaving *mySav = myCt->saving();
+
+	myPar.directory="d:\\huge" ;
+	myPar.prefix="test_";
+	myPar.suffix=".edf";
+	myPar.fileFormat= CtSaving::EDF;
+	myPar.savingMode= CtSaving::AutoFrame;
+	myPar.overwritePolicy= CtSaving::Overwrite;
+	mySav->setParameters(myPar);
+#endif
+
+	struct stcPcoData * m_pcoData = myCam->_getPcoData();
+
+	WORD activeRamSegment = myCam->pcoGetActiveRamSegment();
+
+	msg = "myCam->_getFramesMax(activeRamSegment)" ; printf("\n\n%s> ======================= [%s %d]\r\n", msg, __FILE__, __LINE__);
+	unsigned long  framesMax = myCam->pcoGetFramesMax(activeRamSegment);
+
+	printf("=== %s> activeRamSegment[%d] framesMax[%ld]\n", fnId, activeRamSegment, framesMax);
+	printf("=== %s> cocRunTime[%g] framesRate[%g]\n", fnId, myIf->getCocRunTime(), myIf->getFrameRate() );
+
+
+	int nb_frames1, nb_frames2, nb_acq_frames; 	
+	myCtAcq->getAcqNbFrames(nb_frames1);
+	myCtAcq->setAcqNbFrames(DATA_NR_FRAMES);
+	myCtAcq->getAcqNbFrames(nb_frames2);
+	printf("=== %s> requestedFrames[%d] [%d]\n", fnId, nb_frames1,nb_frames2 );
+
+	double acq_time1, acq_time2;
+	myCtAcq->getAcqExpoTime(acq_time1);
+	myCtAcq->setAcqExpoTime(DATA_EXP_TIME);
+	myCtAcq->getAcqExpoTime(acq_time2);
+	printf("=== %s> expTime[%g] [%g]\n", fnId, acq_time1,acq_time2 );
+
+	msg = "myCt->prepareAcq()" ; printf("\n\n%s> ======================= [%s %d]\r\n", msg, __FILE__, __LINE__);
+	myCt->prepareAcq();
+
+	msg = "startAcq" ; printf("\n\n%s> ======================= [%s %d]\r\n", msg, __FILE__, __LINE__);
+	myCt->startAcq();
+
+	myCt->getStatus(status);
+	printf("\n\n======================================================================================= %s> SLEEP begin\n", fnId );
+
+	while(1) {
+		myCt->getStatus(status);
+		if(status.AcquisitionStatus == AcqReady) {
+			printf("\n\n============================================== %s> [%d] READY\n\n", fnId, status.AcquisitionStatus );
+			break;
+		}
+		if(status.AcquisitionStatus == AcqFault) {
+			printf("\n\n============================================== %s> [%d] ERROR\n\n", fnId,  status.AcquisitionStatus);
+			break;
+		}
+	printf("\n\n [%d]======================================================================================= %s> SLEEP begin\n", status.AcquisitionStatus, fnId );
+		Sleep(0.1);
+	}
+
+	myCtAcq->getAcqNbFrames(nb_acq_frames);
+	printf("=== %s> ACQFrames[%d]\n", fnId, nb_acq_frames );
+
+	msg = "getInfo" ; printf("\n\n%s> ======================= [%s %d]\r\n", msg, __FILE__, __LINE__);
+	//printf("%s\n", myCam->getInfo(info, INFO_SIZE));
+	printf("%s\n", myCam->getInfo());
+
+	msg = "delete cam" ; printf("\n\n%s> ======================= [%s %d]\r\n", msg, __FILE__, __LINE__);
+	delete myCam;
+	msg = "after delete cam" ; printf("\n\n%s> ======================= [%s %d]\r\n", msg, __FILE__, __LINE__);
+
+	PoolThreadMgr::get().quit();
+	msg = "after PoolThreadMgr::get().quit()" ; printf("\n\n%s> ======================= [%s %d]\r\n", msg, __FILE__, __LINE__);
+
+	return 0;
+}
+
+} // lima
+//===============================================================================================================
+//===============================================================================================================
+
+int getlib(void)
+{
+	HINSTANCE SC2Lib = NULL;
+	HINSTANCE LibLimaCore = NULL;
+	HINSTANCE libprocesslib = NULL;
+	HINSTANCE liblimapco = NULL;
+
+	DWORD liberror;
+	SC2Lib = LoadLibrary("SC2_Cam");
+	if (SC2Lib == NULL) {liberror = GetLastError(); return liberror; }
+
+	LibLimaCore = LoadLibrary("LibLimaCore");
+	if (LibLimaCore == NULL) {liberror = GetLastError(); return liberror; }
+
+	libprocesslib = LoadLibrary("libprocesslib");
+	if (libprocesslib == NULL) {liberror = GetLastError(); return liberror; }
+
+	liblimapco = LoadLibrary("liblimapco");
+	if (liblimapco == NULL) {liberror = GetLastError(); return liberror; }
+
+	return FALSE;
+}
+
+int main(int argc, char* argv[]) { return lima::myMain(argc, argv);}
